662cea2a8b0e933c02f784de9b9a7ea5846660f1
TasinHossain05 <tasinhossain399@gmail.com>
2026-02-18 22:31:07 +0800

    Add files via upload

---
+++ week1_q1/src/week1_q1.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : week1_q1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Name: MD. Tofazzal Hossain Tasin" << endl;
+    cout << "Student ID: SCSJ2500606" << endl;
+    cout << "Course Code: DIT1124 (Programming Fundamentals)" << endl;
+
+    return 0;
+}
